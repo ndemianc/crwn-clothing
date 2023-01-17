c29c42dc875419fb04bb4f5e3132a6dfe7d2be40
Sergii Demianchuk <sergeydemjanchyk@gmail.com>
2023-01-17 02:10:31 -0500

    Add router

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
-import CategoryItem from './components/category-item/category-item.component';
-import Directory from './components/directory/directory.component';
+import Navigation from './components/routes/navigation/navigation.component';
+import Home from './components/routes/home/home.component';
+import { Routes, Route, Outlet } from 'react-router-dom';
+
 import './sategories.styles.scss';
 
+const Shop = () => {
+  return <h1>This is SHOP</h1>
+}
+
 const App = () => {   
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ];
- 
   return (
-    <Directory categories={categories}></Directory>
-  )
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
